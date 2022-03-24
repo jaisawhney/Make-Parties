d93db9b06b3ce89248e1ea32e849e19e0515a982
Jai Sawhney <jai.sawhney1@gmail.com>
2022-03-23 18:25:13 -0700

    Display mock events

--- app.js
+++ app.js
@@ -7,11 +7,29 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 app.engine('handlebars', engine({defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 app.set('view engine', 'handlebars');
 
+const events = [
+    {
+        title: "I am your first event",
+        desc: "A great event that is super fun to look at and good",
+        imgUrl: "http://via.placeholder.com/600"
+    },
+    {
+        title: "I am your second event",
+        desc: "A great event that is super fun to look at and good",
+        imgUrl: "http://via.placeholder.com/600"
+    },
+    {
+        title: "I am your third event",
+        desc: "A great event that is super fun to look at and good",
+        imgUrl: "http://via.placeholder.com/600"
+    }
+]
+
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!' });
-})
+    res.render('events-index', {events: events});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('App listening on port 3000!')
-})
+});
